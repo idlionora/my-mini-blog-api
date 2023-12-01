5c8391542f8635821b2204b1399896c2d224fc9c
Nurul Azizah <idlionora@gmail.com>
2023-12-01 19:02:04 +0700

    feat:(cookie-parser): install cookie-parser, add logout route to overwrite persisted jwt token in browser

--- functions/routes/userRoutes.js
+++ functions/routes/userRoutes.js
@@ -11,6 +11,7 @@ router.use("/:userId/comments", commentRouter);
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.get("/logout", authController.logout);
 
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
