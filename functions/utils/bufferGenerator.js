052ca89c1534d75510562736146304358786cd58
Nurul Azizah <idlionora@gmail.com>
2023-12-09 23:01:12 +0700

    refactor(bufferGenerator): add functions regarding image buffer to BufferGenerator class with image source as instance

---
+++ functions/utils/bufferGenerator.js
@@ -0,0 +1,50 @@
+const sharp = require("sharp");
+
+function parseImgSize({ width, height, orientation }) {
+  return (orientation || 0) >= 5
+    ? { width: height, height: width }
+    : { width, height };
+}
+
+class BufferGenerator {
+  constructor(bufferSource) {
+    this.bufferSource = bufferSource;
+  }
+
+  async getSourceImgSize() {
+    return parseImgSize(await sharp(this.bufferSource).metadata());
+  }
+
+  async getSharpBuffer(outputSize, fitSetting) {
+    const { width, height } = outputSize;
+
+    return await sharp(this.bufferSource)
+      .resize(width, height, { fit: fitSetting })
+      .toFormat("jpeg")
+      .jpeg({ quality: 90, mozjpeg: true })
+      .keepExif()
+      .toBuffer();
+  }
+
+  async generateImgBufferPrioritizeWidth(outputSize) {
+    const imgSize = await this.getSourceImgSize();
+    const aspectRatio = outputSize.width / outputSize.height;
+
+    if (imgSize.width / imgSize.height <= aspectRatio) {
+      return await this.getSharpBuffer(outputSize, "cover");
+    }
+    return await this.getSharpBuffer(outputSize, "inside");
+  }
+
+  async generateImgBufferPrioritizeHeight(outputSize) {
+    const imgSize = await this.getSourceImgSize();
+    const aspectRatio = outputSize.width / outputSize.height;
+
+    if (imgSize.width / imgSize.height >= aspectRatio) {
+      return await this.getSharpBuffer(outputSize, "cover");
+    }
+    return await this.getSharpBuffer(outputSize, "inside");
+  }
+}
+
+module.exports = BufferGenerator;
